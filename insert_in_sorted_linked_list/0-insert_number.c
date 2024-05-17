1ce0c053278a9441ca0c5cd21aab3ad5aa4364f8
shaw342 <shawanbarua70@gmail.com>
2024-05-17 22:33:07 +0200

    change condition

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -8,9 +8,15 @@ listint_t *insert_node(listint_t **head, int number)
   new = malloc(sizeof(listint_t));
   if (new == NULL){
 
-    return exit(-1);
+    return NULL;
   }
   new->n = number;
+  new->next = NULL;
+  if (*head == NULL || (*head)->n >= number)
+  {
+    new->next = *head;
+    *head = new;
+  }
   while(current->next != NULL && current->next->n < number)
   {
     current= current->next;
