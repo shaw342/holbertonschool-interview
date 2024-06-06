227ad86ddcbcd512e1183c2a3e1d865baa92c4fa
shaw342 <shawanbarua70@gmail.com>
2024-06-06 12:34:09 +0200

    betty

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,6 +1,7 @@
 #include "sandpiles.h"
 #include <stdio.h>
-/*
+
+/**
  * is_stable - checks if sandpile is stable.
  * @grid: grid to check
  * Return: 1 if stable else 0
@@ -8,16 +9,17 @@
 
 int is_stable(int grid[3][3])
 {
-    int i, j;
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-                return (0);
-        }
-    }
-    return (1);
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (0);
+		}
+	}
+	return (1);
 }
 
 /**
@@ -27,44 +29,43 @@ int is_stable(int grid[3][3])
 
 void topple(int grid[3][3])
 {
-    int tempGrid[3][3];
-    int i, j;
+	int tempGrid[3][3];
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            tempGrid[i][j] = 0;
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			tempGrid[i][j] = 0;
+		}
+	}
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-            {
-                tempGrid[i][j] -= 4;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				tempGrid[i][j] -= 4;
 
-                if (i - 1 >= 0)
-                    tempGrid[i - 1][j]++;
-                if (i + 1 < 3)
-                    tempGrid[i + 1][j]++;
-                if (j - 1 >= 0)
-                    tempGrid[i][j - 1]++;
-                if (j + 1 < 3)
-                    tempGrid[i][j + 1]++;
-            }
-        }
-    }
+				if (i - 1 >= 0)
+					tempGrid[i - 1][j]++;
+				if (i + 1 < 3)
+					tempGrid[i + 1][j]++;
+				if (j - 1 >= 0)
+					tempGrid[i][j - 1]++;
+				if (j + 1 < 3)
+					tempGrid[i][j + 1]++;
+			}
+		}
+	}
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid[i][j] += tempGrid[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid[i][j] += tempGrid[i][j];
+		}
+	}
 }
 
 /**
@@ -75,27 +76,28 @@ void topple(int grid[3][3])
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
-    while (!is_stable(grid1))
-    {
-        printf("=\n");
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (j)
-                    printf(" ");
-                printf("%d", grid1[i][j]);
-            }
-            printf("\n");
-        }
-        topple(grid1);
-    }
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
+	while (!is_stable(grid1))
+	{
+		printf("=\n");
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (j)
+					printf(" ");
+				printf("%d", grid1[i][j]);
+			}
+			printf("\n");
+		}
+		topple(grid1);
+	}
 }
