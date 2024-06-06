4731dc0ada9c033264d616ee0e3f43903cca3116
shaw342 <shawanbarua70@gmail.com>
2024-06-06 11:53:29 +0200

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,84 +1,101 @@
-#include <stdlib.h>
-#include <stdio.h>
-
 #include "sandpiles.h"
+#include <stdio.h>
+/*
+ * is_stable - checks if sandpile is stable.
+ * @grid: grid to check
+ * Return: 1 if stable else 0
+ */
+
+int is_stable(int grid[3][3])
+{
+    int i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+                return (0);
+        }
+    }
+    return (1);
+}
 
 /**
- * print_grid_sum - Print 3x3 grids sum
- * @grid1: Left 3x3 grid
- * @grid2: Right 3x3 grid
- *
+ * topple - topples the sandpile to distribute sand
+ * @grid: grid to topple
  */
-static void print_grid_sum(int grid1[3][3], int grid2[3][3])
+
+void topple(int grid[3][3])
 {
+    int tempGrid[3][3];
     int i, j;
 
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
         {
-            if (j)
-                printf(" ");
-            printf("%d", grid1[i][j]);
+            tempGrid[i][j] = 0;
         }
-
-        printf(" %c ", (i == 1 ? '+' : ' '));
-
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid2[i][j]);
-        }
-        printf("\n");
     }
-}
-
-/**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
- *
- */
-static void print_grid(int grid[3][3])
-{
-    int i, j;
 
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
         {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
+            if (grid[i][j] > 3)
+            {
+                tempGrid[i][j] -= 4;
+
+                if (i - 1 >= 0)
+                    tempGrid[i - 1][j]++;
+                if (i + 1 < 3)
+                    tempGrid[i + 1][j]++;
+                if (j - 1 >= 0)
+                    tempGrid[i][j - 1]++;
+                if (j + 1 < 3)
+                    tempGrid[i][j + 1]++;
+            }
         }
-        printf("\n");
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid[i][j] += tempGrid[i][j];
+        }
     }
 }
 
 /**
- * main - Entry point
- *
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * sandpiles_sum - compute the sum of two sandpiles.
+ * @grid1: first sandpile
+ * @grid2: second sandpile
  */
-int main(void)
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int grid1[3][3] = {
-        {3, 3, 3},
-        {3, 3, 3},
-        {3, 3, 3}
-    };
-    int grid2[3][3] = {
-        {1, 3, 1},
-        {3, 3, 3},
-        {1, 3, 1}
-    };
-
-    print_grid_sum(grid1, grid2);
-
-    sandpiles_sum(grid1, grid2);
-
-    printf("=\n");
-    print_grid(grid1);
-
-    return (EXIT_SUCCESS);
+    int i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid1[i][j] += grid2[i][j];
+        }
+    }
+    while (!is_stable(grid1))
+    {
+        printf("=\n");
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                if (j)
+                    printf(" ");
+                printf("%d", grid1[i][j]);
+            }
+            printf("\n");
+        }
+        topple(grid1);
+    }
 }
