c2c4f942db63ba1da0e9e7788582a3bb3450ffef
shaw342 <shawanbarua70@gmail.com>
2024-06-14 16:47:24 +0200

    palindrom

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -2,11 +2,6 @@
 
 int is_palindrome(listint_t **head)
 {
-	 if (*head == NULL || (*head)->next == NULL) {
-		return 1;
-	 }
-
-
 	listint_t *slow = *head;
 	listint_t *fast = *head;
 	listint_t *reverseHead = NULL;
