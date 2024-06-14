fc6ad1e72e6d9ad10e15c7e9a7f7f6c4523dad92
shaw342 <shawanbarua70@gmail.com>
2024-06-14 16:37:03 +0200

    palindrome

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,60 @@
+#include "lists.h"
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *reverseHead = NULL;
+	int result;
+
+    
+    	while (fast != NULL && fast->next != NULL) {
+		slow = slow->next;
+        	fast = fast->next->next;
+	}
+
+	reverseHead = reverse(slow->next);
+	slow->next = NULL;
+	result = cmp(*head, reverseHead);
+
+	reverse(reverseHead);
+	slow->next = reverseHead;
+
+	return result;
+}
+
+int cmp(listint_t *head1,listint_t *head2) 
+{
+	listint_t *list1 = head1;
+	listint_t *list2 = head2;
+
+	while (list1 != NULL && list2 != NULL)
+	{
+		if (list1->n != list2->n)
+			return 0;
+
+		list1 = list1->next;
+		list2 = list2->next;
+	}
+
+	return 1;
+}
+
+listint_t *reverse(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+
+		prev = current;
+		current = next;
+	}
+
+	return prev;
+}
