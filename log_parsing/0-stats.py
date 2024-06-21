d89f0a8aec5cdc4277611256a0ef3b6ab4b8022e
shaw342 <shawanbarua70@gmail.com>
2024-06-21 17:00:36 +0200

    stats

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import sys
+
+# Initialize variables
+codes = {}
+status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+count = 0
+size = 0
+
+
+def print_statistics(size, codes):
+    print("File size: {}".format(size))
+    for key in sorted(codes):
+        print("{}: {}".format(key, codes[key]))
+
+
+try:
+    for ln in sys.stdin:
+        if count == 10:
+            print_statistics(size, codes)
+            count = 1
+        else:
+            count += 1
+
+        ln = ln.split()
+
+        try:
+            size = size + int(ln[-1])
+        except (IndexError, ValueError):
+            pass
+
+        try:
+            if ln[-2] in status_codes:
+                if ln[-2] not in codes:
+                    codes[ln[-2]] = 1
+                else:
+                    codes[ln[-2]] += 1
+        except IndexError:
+            pass
+
+    print_statistics(size, codes)
+
+except KeyboardInterrupt:
+    print_statistics(size, codes)
+    raise
