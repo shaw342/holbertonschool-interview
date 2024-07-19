8148f84a984ad89fe88ee8ea8f114f98a020c5ed
shaw342 <shawanbarua70@gmail.com>
2024-07-19 14:37:03 +0200

    0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+"""
+ValidUTF8 Module
+"""
 import codecs
 
 
 def validUTF8(data):
-
+    """
+    Determine if a given data set represent a valid UTF-8 encoding
+    """
     for num in data:
         if num < 0 or num > 255:
             return False
     try:
-        codecs.decode(bytes(data),"utf-8")
+        codecs.decode(bytes(data), "utf-8")
         return True
     except UnicodeDecodeError:
         return False
