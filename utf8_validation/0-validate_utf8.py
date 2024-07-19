c5249fea35197b93c35d651033df86d46bb4019e
shaw342 <shawanbarua70@gmail.com>
2024-07-19 14:27:32 +0200

    push for algo

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+import codecs
+
+
+def validUTF8(data):
+
+    for num in data:
+        if num < 0 or num > 255:
+            return False
+    try:
+        codecs.decode(bytes(data),"utf-8")
+        return True
+    except UnicodeDecodeError:
+        return False
