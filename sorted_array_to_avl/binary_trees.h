f29fe4832591820ec39233525dff4ee2621c4d9b
shaw342 <shawanbarua70@gmail.com>
2024-08-09 23:53:11 +0200

    add prototype in header file

--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -2,8 +2,8 @@
 #define _BINARY_TREES_H_
 
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
+
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,16 +13,21 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
+
+/* Prototype Functions */
 
 void binary_tree_print(const binary_tree_t *);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *binary_tree_node(avl_t *parent, int index, int *array);
+avl_t *SortedArrayToAVL(int *array, size_t start, size_t end, avl_t *root);
 
 #endif /* _BINARY_TREES_H_ */
