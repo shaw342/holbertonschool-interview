d2af27538a1cd1537a711289cc812052f6738525
shaw342 <shawanbarua70@gmail.com>
2024-08-30 16:40:54 +0200

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+
+"""Module that calculate how many square units of water
+    will be retained after it rains."""
+
+
 def rain(walls):
     """
     rain module square_water
