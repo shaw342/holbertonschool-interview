94b3fb4385f7a44a1ce766c594683ea9e9e28c9d
shaw342 <shawanbarua70@gmail.com>
2024-09-13 17:09:36 +0200

    0-advanced_binary.c

---
+++ advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+void printArray(int *array,int low,int high){
+	int i;
+	printf("Searching in array:");
+	for (i = low; i < high;i++){
+		printf(" %d",array[i]);
+	}
+	printf("\n");
+}
+
+int recursiveSearch(int *array,int low, int high,int value){	
+	int mid;
+
+	if (low > high)
+		return (-1);
+
+	printArray(array, low,high);
+
+	mid = low + (high - low) / 2;
+
+	if (array[mid] == value && (mid == low || array[mid - 1] != value))
+		return (mid);
+
+	if (array[mid] >= value)
+		return (recursiveSearch(array, low, mid, value));
+	return (recursiveSearch(array, mid + 1, high, value));
+}
+int advanced_binary(int *array, size_t size, int value){
+
+	if (array == NULL){
+		return -1;
+	}
+
+	return recursiveSearch(array,0, size -1,value);
+}
---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef BINARY_SEARCH
+#define BINARY_SEARCH
+int advanced_binary(int *array, size_t size, int value);
+void printArray(int *array,int low,int high);
+int recursiveSearch(int *array,int low, int high,int value);
+#endif
