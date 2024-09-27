f75f0f09c909b2b9da13bf40474b38d512e49f6b
shaw342 <shawanbarua70@gmail.com>
2024-09-27 19:32:49 +0200

    add header file

--- heap_extract/binary_trees.h
+++ heap_extract/binary_trees.h
@@ -1,3 +1,9 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+#include "stdio.h"
+#include "stdlib.h"
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,4 +23,13 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
 
-void binary_tree_print(const binary_tree_t *);
+
+int heap_extract(heap_t **root);
+size_t binary_tree_size(const binary_tree_t *tree);
+void heapify(int *array, size_t size);
+size_t binary_tree_height(heap_t *tree);
+heap_t *push_to_top(heap_t *root);
+void heapify_down(heap_t *root);
+
+
+#endif /* BINARY_TREE_H */
