a6dbdb843f8daf6cd6c7077b9a2e633ab393e55d
shaw342 <shawanbarua70@gmail.com>
2024-10-25 21:55:24 +0200

    making change

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-make change file
+Determine the fewest number of coins needed to meet a given amount total
 """
 
 
@@ -8,15 +8,15 @@ def makeChange(coins, total):
     """
     Return the fewest number of coins needed to meet total
     """
-    coin_reverse = sorted(coins, reverse=True)
-    count = 0
+    temp_value = 0
+    coins.sort(reverse=True)
 
-    if total <= 0:
+    if total < 0:
         return 0
 
-    for coin in coin_reverse:
+    for coin in coins:
         if total % coin <= total:
-            count += total // coin
+            temp_value += total // coin
             total = total % coin
 
-    return count if total == 0 else -1
+    return temp_value if total == 0 else -1
