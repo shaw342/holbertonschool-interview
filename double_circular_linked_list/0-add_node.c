1219e2815096d8bafc6c2b1c6ed2cd137d77624c
shaw342 <shawanbarua70@gmail.com>
2024-10-31 20:28:24 +0100

    add node

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -1,40 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "list.h"
 
-List *add_node_end(List **list, char *str){
-	List *newNode;
-	List *last = *list;
-
-	newNode = (List*) malloc(sizeof(List));
-
-	if (newNode == NULL){
-
-		return (NULL);
-	}
-	newNode->str = str;
-	newNode->next = NULL;
-
-	while (last->next != NULL){
-		last = last->next;
-	}
-	last->next = newNode;
-	newNode->prev = last;
-
-	return (*newNode)
+// Function to create a new node
+List *create_node(char *data) {
+    List *newNode = (List *)malloc(sizeof(List));
+    if (newNode == NULL) {
+        return NULL;
+    }
+    newNode->str = strdup(data);
+    if (newNode->str == NULL) {
+        free(newNode);
+        return NULL;
+    }
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
 }
 
-List *add_node_begin(List **list, char *str){
-	List *newNode;
-	List *head = *list;
-	newNode = (List*) malloc(sizeof(List));
+// Function to add a new node to the end of the list
+List *add_node_end(List **list, char *str) {
+    List *newNode = create_node(str);
+    if (newNode == NULL) {
+        return NULL;
+    }
 
-	if (newNode == NULL){
-		return (NULL);
-	}
+    if (*list == NULL) {
+        // If the list is empty, initialize it with the new node
+        newNode->next = newNode;
+        newNode->prev = newNode;
+        *list = newNode;
+    } else {
+        // Adjust the pointers to add the new node at the end
+        List *last = (*list)->prev;
+        newNode->next = *list;
+        newNode->prev = last;
+        last->next = newNode;
+        (*list)->prev = newNode;
+    }
 
-	newNode->str = str;
-	newNode->next = NULL;
-	head->next = newNode;
-	newNode->prev = head;
-
-	return (*newNode);
+    return newNode;
 }
+
+// Function to add a new node to the beginning of the list
+List *add_node_begin(List **list, char *str) {
+    List *newNode = create_node(str);
+    if (newNode == NULL) {
+        return NULL;
+    }
+
+    if (*list == NULL) {
+        // If the list is empty, initialize it with the new node
+        newNode->next = newNode;
+        newNode->prev = newNode;
+        *list = newNode;
+    } else {
+        // Adjust the pointers to add the new node at the beginning
+        List *last = (*list)->prev;
+        newNode->next = *list;
+        newNode->prev = last;
+        last->next = newNode;
+        (*list)->prev = newNode;
+        *list = newNode;
+    }
+
+    return newNode;
+}
+
