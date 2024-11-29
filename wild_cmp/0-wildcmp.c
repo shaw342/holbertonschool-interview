725ebd44fb3e8a58aa85603986dfbaa4198c95a2
shaw342 <shawanbarua70@gmail.com>
2024-11-29 16:31:36 +0100

    betty code style

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
+/**
+ * wildcmp - Compares two strings and returns 1 if they can be
+ * considered identical, otherwise returns 0.
+ * @s1: The first string.
+ * @s2: The second string, which can contain the special character *.
+ *
+ * Return: 1 if the strings are identical, 0 otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
 
-int wildcmp(char *s1, char *s2) {
-    // If both strings are empty, they are identical
-    if (*s1 == '\0' && *s2 == '\0') {
-        return 1;
-    }
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	}
 
-    // If s2 has a *, then we have two options:
-    // 1. Ignore the * and move to the next character in s2
-    // 2. Match the * with the current character in s1 and move to the next character in s1
-    if (*s2 == '*') {
-        return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-    }
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
 
-    // If the current characters of s1 and s2 match, move to the next characters
-    if (*s1 == *s2) {
-        return wildcmp(s1 + 1, s2 + 1);
-    }
-
-    // If none of the above conditions are met, the strings are not identical
-    return 0;
+	return (0);
 }
