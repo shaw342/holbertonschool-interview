425a1522b4992d8ba4e774f8c92b073e09c5046f
shaw342 <shawanbarua70@gmail.com>
2024-12-05 12:11:17 +0100

    pycode style

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
     module pascal_triangle
 """
 
+
 def pascal_triangle(n):
     """
         function take n integer
@@ -10,12 +11,13 @@ def pascal_triangle(n):
     """
 
     T = []
+
     for i in range(n):
-        l = [0] * (i + 1)
+        L = [0] * (i + 1)
         for j in range(i+1):
             if j != 0 and j != i:
-                l[j] =  T[i-1][j-1] + T[i-1][j]
+                L[j] = T[i-1][j-1] + T[i-1][j]
             else:
-                l[j] = 1
-        T.append(l)
+                L[j] = 1
+        T.append(L)
     return T
