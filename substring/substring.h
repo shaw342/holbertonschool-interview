168083904e15f9de299b634cbb5c268861af7b50
shaw342 <shawanbarua70@gmail.com>
2024-12-20 21:20:45 +0100

    forget change header file

--- substring/substring.h
+++ substring/substring.h
@@ -1,5 +1,9 @@
-#ifndef __SUBSTRING__
-#define __SUBSTRING__
-#include <string.h>
+#ifndef SUBSTRING_H
+#define SUBSTRING_H
+
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
-#endif // !DEBUG
+void count_word_frequency(char const **words, int nb_words, int *expected_count);
+int is_valid_substring(const char *s, int start, int word_len, int nb_words,
+	const char **words, int *expected_count);
+
+#endif /* SUBSTRING_H */
